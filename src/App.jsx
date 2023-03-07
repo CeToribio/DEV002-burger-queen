8b66b27ffbe668e46433d9813877b99498fcc7d1
Ana Cecilia Toribio Arrieta <cecitoribio@MacBook-Pro-de-Ana.local>
2023-03-06 23:33:32 -0500

    pruebas de los primeros compenentes de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+// export function Product({ product, cost, data}) {
+//   console.log(data)
+//   return (
+//     <div>
+//       <button>
+//         <p>{product}</p>
+//         <p>{cost}</p>
+//       </button>
+//     </div>
+//   )
+// }
 
+export function Product({ products, type }) {
+  const show = type ? "hamburguesa" : "no"
+  console.log(show)
+  const { name, cost } = products
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <button>
+        <p>{name}</p>
+        <p>{cost}</p>
+      </button>
     </div>
   )
 }
 
-export default App
+export function Menu({ data }) {
+  //const state = useState(false)
+  //devuelve un array con dos posiciones false y true(formatear)
+  // console.log(state)
+  // const choose = state[0]
+  // const setChoose = state[1]
+  const [choose, setChoose] = useState(false)
+
+  const text = choose ? "seleccionado" : "no seleccionado"
+  console.log(text)
+
+  const handleClick = () => {
+    setChoose(!choose)
+  }
+
+  const { name, img } = data
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <img src={img} alt="imagen de la elección" />
+        <p>{name}</p>
+      </button>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Product, Menu } from './App'
+import hamburguesa from './hamburguesa.png'
+import bebida from './agua.png'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const hamburguesas = { name: "HAMBURGUESAS", img: "hamburguesa" }
+const bebidas = { name: "BEBIDAS", img: "bebida" }
+
+//const productoPrueba = { name: "Hamburguesa", cost: "10" }
+const hCarne = { name: "Hamburguesa de carne ", cost: "s/.15" }
+const hPollo = { name: "Hamburguesa de pollo ", cost: "s/.15" }
+const hVegetariana = { name: "Hamburguesa vegetariana ", cost: "s/.15" }
+const hMixto = { name: "Hamburguesa mixta ", cost: "s/.20" }
+const papas = { name: "papas fritas", cost: "s/.10" }
+
+const cafeAmericano = { name: "Café americano ", cost: "5" }
+const gaseosaL = { name: "Gaseosa 1L", cost: "15" }
+const gaseosaMl = { name: "Gaseosa 500 ml", cost: "10" }
+const aguaL = { name: "Agua mineral 1L", cost: "15" }
+const aguaMl = { name: "Agua mineral 500 ml", cost: "10" }
+
+//crear los objetos para luego renderizarlos
+
+root.render(
+  <>
+    <p>Hola</p>
+    <Menu data={hamburguesas} />
+    <Menu data={bebidas} />
+    {/* <Product product={"Agua 1l"} cost={"s/. 10"} /> */}
+    <Product products={hCarne} type={hamburguesa} />
+    <Product products={hPollo} type={hamburguesa} />
+    <Product products={hVegetariana} type={hamburguesa}  />
+    <Product products={hMixto} type={hamburguesa} />
+    <Product products={papas} type={hamburguesa}  />
+    <Product products={cafeAmericano} />
+    <Product products={gaseosaL} />
+    <Product products={gaseosaMl} />
+    <Product products={aguaL} />
+    <Product products={aguaMl} />
+
+  </>
 )
+
